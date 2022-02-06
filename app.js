848e20d4c8c914fb30e770aab2358a757bb9cd84
Melkias Abraham <melkiasae@hotmail.com>
2022-02-05 20:53:56 -0500

    Added the document event to load up all the HTML tags

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userGrid = document.querySelector(".grid-user");
+  const computerGrid = document.querySelector(".grid-computer");
+  const displayGrid = document.querySelector(".grid-display");
+  const ships = document.querySelectorAll(".ship");
+  const destroyer = document.querySelector(".destroyer-container");
+  const submarine = document.querySelector(".submarine-container");
+  const crusier = document.querySelector(".crusier-container");
+  const battleship = document.querySelector(".battleshiper-container");
+  const carrier = document.querySelector(".carrier-container");
+
+  const startButton = document.querySelector('#start')
+  const rotateButton = document.querySelector('#rotate')
+  const turnDisplay = document.querySelector('#whose-turn')
+  const infoDisplay = document.querySelector('#game-info')
+});
