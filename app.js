3dbd32f6a99c00f91e5903aa755e684a5c90340d
Melkias Abraham <melkiasae@hotmail.com>
2022-02-07 19:34:37 -0500

    100 seperate squares for cordinates now generated in the user and computer boards

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  
   const userGrid = document.querySelector(".grid-user");
   const computerGrid = document.querySelector(".grid-computer");
   const displayGrid = document.querySelector(".grid-display");
@@ -9,8 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const battleship = document.querySelector(".battleshiper-container");
   const carrier = document.querySelector(".carrier-container");
 
+  
   const startButton = document.querySelector('#start')
   const rotateButton = document.querySelector('#rotate')
   const turnDisplay = document.querySelector('#whose-turn')
   const infoDisplay = document.querySelector('#game-info')
+  
+  // Creating the board
+  const userSquare = [];
+  const computerSquare = [];
+  const width = 10
+
+  function createBoard(grid, squares, width) {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div')
+      square.dataset.id = i
+      grid.appendChild(square)
+      squares.push(square)
+    }
+  }
+
+  createBoard(userGrid, userSquare);
+  createBoard(computerGrid, computerSquare);
 });
