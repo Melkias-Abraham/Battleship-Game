e85ff6f4909e8ce007114738ca63b394a77fdc26
Melkias Abraham <melkiasae@hotmail.com>
2022-02-13 16:21:10 -0500

    created a source and CSS folder for better organization

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
   const userGrid = document.querySelector(".grid-user");
   const computerGrid = document.querySelector(".grid-computer");
   const displayGrid = document.querySelector(".grid-display");
