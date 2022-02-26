3c75c50770aecadcf54a7f660c48b7862bb4cab0
Melkias Abraham <melkiasae@hotmail.com>
2022-02-26 15:15:37 -0500

    Server is now up and running

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ const http = require('http')
 const PORT = process.env.PORT || 3000
 const socketio = require('socket.io')
 const app = express()
-const server = http.createServer(app)
+const server = http.createServer(app) 
 const io = socketio(server)
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")))
+
+// Start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
