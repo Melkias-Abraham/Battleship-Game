dc205aaf65fecc55df9c371ad52ced1ac08893b9
Melkias Abraham <melkiasae@hotmail.com>
2022-02-26 15:35:33 -0500

    Set the amount of players to 2 for multiplayer.

--- server.js
+++ server.js
@@ -13,7 +13,28 @@ app.use(express.static(path.join(__dirname, "src")))
 // Start server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
+
 // Handle a socket connection request from web client
+const connections = [null, null]
+
 io.on('connection', socket => {
   console.log('New web connection')
+
+  // Find an avaliable player number
+  let playerIndex = -1;
+  for (const i in connections) {
+    if (connection[i] === null) {
+      playerIndex = i
+      break
+    }
+  }
+
+  // Ignore player 3
+  if (playerIndex === -1) return
+
+  // Tell the connection client what player number they are
+  socket.emit('player-number', playerIndex)
+
+  console.log(`Player ${playerIndex} has connected`)
 })
+
